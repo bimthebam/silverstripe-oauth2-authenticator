8d998308394d23bca17c07a63b6c02b84af7bdc6
Christian Bünte <buente@buenteweb.de>
2020-06-18 22:16:17 +0200

    Namespace updated, annotations added

--- src/Control/CMS/Admin.php
+++ src/Control/CMS/Admin.php
@@ -1,21 +1,40 @@
 <?php
 
-namespace BimTheBam\OAuth2Authenticator\CMS;
+namespace BimTheBam\OAuth2Authenticator\Control\CMS;
 
 use BimTheBam\OAuth2Authenticator\Model\OAuth2\Provider;
 use SilverStripe\Admin\ModelAdmin;
 
+/**
+ * Class Admin
+ * @package BimTheBam\OAuth2Authenticator\Control\CMS
+ */
 class Admin extends ModelAdmin
 {
+    /**
+     * @var string
+     */
     private static $url_segment = 'oauth2';
 
+    /**
+     * @var string
+     */
     private static $menu_title = 'OAuth2';
 
+    /**
+     * @var string
+     */
     private static $menu_icon_class = 'font-icon-external-link';
 
+    /**
+     * @var string[]
+     */
     private static $managed_models = [
         Provider::class,
     ];
 
+    /**
+     * @var bool
+     */
     public $showImportForm = false;
 }
